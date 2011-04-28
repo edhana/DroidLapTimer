ee36e4b062660886f935180ab3b6f0dde052f752
edhana <edhana@gmail.com>
2011-04-28 06:35:58 -0300

    Initial commit

---
+++ src/br/com/uana/timingR/activities/ChronometerActivity.java
@@ -0,0 +1,14 @@
+package br.com.uana.timingR.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChronometerActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+		// TODO: Design de activity content
+	}
+}
---
+++ src/br/com/uana/timingR/activities/MainWindow.java
@@ -0,0 +1,33 @@
+package br.com.uana.timingR.activities;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.TabHost;
+import br.com.uana.timingR.R;
+
+/**
+ * 
+ * @author Eduardo Marques
+ * @since 28/4/2011
+ * @description Entry point for LapTimer Application for android.
+ */
+public class MainWindow extends TabActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        
+        Resources resources = getResources(); // Get Drawables of the resource obj.
+        TabHost tabHost = getTabHost(); // Activity tabhost
+        TabHost.TabSpec tabSpec; // Reusable tableSpec
+        Intent intent;
+        
+        // Setup and add each tab to tabhost
+        
+        
+    }
+}
